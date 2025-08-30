afa8b92608eed1eb9fba7b6b3f97f5d73c767f18
vedant hande <vedanthande453@gmail.com>
2025-08-30 22:47:28 +0530

    Refactor App.js for consistency in formatting and spacing

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV != "production") {
-  require('dotenv').config();
+  require("dotenv").config();
 }
 const express = require("express");
 const ExpressErr = require("./utils/ExpressErr.js");
@@ -9,19 +9,19 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
 const session = require("express-session");
-const MongoStore = require("connect-mongo"); 
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
-const listings =require("./routers/listings.js");
-const reviews =require("./routers/reviews.js");
-const userrouter =require("./routers/user.js");
+const listings = require("./routers/listings.js");
+const reviews = require("./routers/reviews.js");
+const userrouter = require("./routers/user.js");
 
 const passport = require("passport");
-const Localstrategy= require("passport-local");
+const Localstrategy = require("passport-local");
 const User = require("./models/user.js");
 
 // let MongoURl = "mongodb://127.0.0.1:27017/wanderlust";
 
-const dbUrl= process.env.ATLASDB_URL;
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -35,8 +35,6 @@ async function main() {
   await mongoose.connect(dbUrl);
 }
 
-
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -44,52 +42,47 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsmate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-
-const store =MongoStore.create({
-  mongoUrl : dbUrl,
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
   crypto: {
-    secret:process.env.SECRET,
+    secret: process.env.SECRET,
   },
-  touchAfter:24*3600,
-})
-store.on("error" , ()=>{
-  console.log("ERROR in Mongo session store",err);
+  touchAfter: 24 * 3600,
+});
+store.on("error", () => {
+  console.log("ERROR in Mongo session store", err);
 });
 
-const sessionOptions={
+const sessionOptions = {
   store,
-  secret : process.env.SECRET,
-  resave : false,
+  secret: process.env.SECRET,
+  resave: false,
   saveUninitialized: true,
-  cookie:{
-    expires : Date.now()+7*24*60*60*1000,
-    maxAge : 7*24*60*60*1000,
-    httpOnly :true,
-  }
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 app.use(session(sessionOptions)); //express session : use cookies to make the site sort of stateful
-app.use(flash());//used to display some messages for a some milli second
+app.use(flash()); //used to display some messages for a some milli second
 
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new Localstrategy(User.authenticate()));//user should be authenticate through  authenticate 
-passport.serializeUser(User.serializeUser());//storing all the data of the user in session can be said as serilization
-passport.deserializeUser(User.deserializeUser());  //unstoring all the data of the user from session can be said as deserilization
-
-
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new Localstrategy(User.authenticate())); //user should be authenticate through  authenticate
+passport.serializeUser(User.serializeUser()); //storing all the data of the user in session can be said as serilization
+passport.deserializeUser(User.deserializeUser()); //unstoring all the data of the user from session can be said as deserilization
 
 // app.get("/", (req, res) => {
 //   res.send("hi i am root");
 // });
 
-
-
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.Curruser = req.user;
-  next(); 
-})
+  next();
+});
 // app.use((err, req, res, next) => {
 //   let { statusCode = 500, message = "Something went wrong!" } = err;
 // console.error(err);
@@ -105,10 +98,9 @@ app.use((req,res,next)=>{
 //   res.send(registerUser);
 // })
 
-
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews)
-app.use("/",userrouter)
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
+app.use("/", userrouter);
 
 app.get("/", (req, res) => {
   res.redirect("/listings");

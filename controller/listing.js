b2d799768114015c3241f7bcff2d1e93bab4b6bd
harsh882233 <harsh882233@gmail.com>
2025-08-30 23:08:39 +0530

    Fix path for Listing model require

--- controller/listing.js
+++ controller/listing.js
@@ -1,4 +1,5 @@
-const Listing = require("C:/Users/Lenovo/OneDrive/backend/Major Project/models/listing.js");
+const Listing = require("./models/listing");
+
 
 module.exports.index= async (req, res) => {
     const allListings = await Listing.find({});
